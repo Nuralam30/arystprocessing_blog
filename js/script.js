aa2ed460ed6a415091a0f2b22922ac846b3db712
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-11 22:38:39 +0600

    updated some code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 
+// sticky navbar in the top
 document.addEventListener("DOMContentLoaded", function(){
     window.addEventListener('scroll', function() {
         if (window.scrollY > 0) {
           document.getElementById('navbar_top').classList.add('fixed-top');
-          // add padding top to show content behind navbar
+         
           navbar_height = document.querySelector('.nav-bar').offsetHeight;
           document.body.style.paddingTop = navbar_height + 'px';
           document.querySelector('.nav-bar').style.background = '#fff';
           document.querySelector('.nav-bar').style.boxShadow = '5px 10px 15px #dedede';
         } else {
           document.getElementById('navbar_top').classList.remove('fixed-top');
-           // remove padding top from body
+           
           document.body.style.paddingTop = '0';
           document.querySelector('.nav-bar').style.background = 'none';
           document.querySelector('.nav-bar').style.boxShadow = 'none';
         } 
     });
   });
+
+
+    // see more button event listener
+    const textContent = document.querySelectorAll('.content-text').childnodes;
+    
+    textContent.addEventListener('click', function(e){
+      const seeMoreBtn = e.target;
+      console.log(seeMoreBtn)
+    })
+
+
