011f63a953397cb0b1ab0e6641a427292954fe80
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-17 00:36:49 +0600

    fully complete the project

--- js/script.js
+++ js/script.js
@@ -2,29 +2,57 @@
 // sticky navbar in the top
 document.addEventListener("DOMContentLoaded", function(){
     window.addEventListener('scroll', function() {
-        if (window.scrollY > 0) {
-          document.getElementById('navbar_top').classList.add('fixed-top');
+      if (window.scrollY > 0) {
+        scrollplay();  
+        document.querySelector('.nav-bar').style.background = '#fff';
+        document.querySelector('.nav-bar').style.boxShadow = '5px 10px 15px #dedede';
+      }
+      else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
          
-          navbar_height = document.querySelector('.nav-bar').offsetHeight;
-          document.body.style.paddingTop = navbar_height + 'px';
-          document.querySelector('.nav-bar').style.background = '#fff';
-          document.querySelector('.nav-bar').style.boxShadow = '5px 10px 15px #dedede';
-        } else {
-          document.getElementById('navbar_top').classList.remove('fixed-top');
-           
-          document.body.style.paddingTop = '0';
-          document.querySelector('.nav-bar').style.background = 'none';
-          document.querySelector('.nav-bar').style.boxShadow = 'none';
-        } 
+        document.body.style.paddingTop = '0';
+        document.querySelector('.nav-bar').style.background = 'none';
+        document.querySelector('.nav-bar').style.boxShadow = 'none';
+      } 
     });
   });
+
+
+  // function scrollplay event
+  function scrollplay(){
+      document.getElementById('navbar_top').classList.add('fixed-top');
+     
+      navbar_height = document.querySelector('.nav-bar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+  }
+
+
+   
+    //============== responsive design ============
+    function responsive(){
+      var navBar = document.getElementById('navbar_top');
+
+      if (navBar.className === 'nav-bar') {
+        navBar.className += ' responsive';
+
+        window.addEventListener('scroll', function() {
+          navBar.className = 'nav-bar';
+          if (window.scrollY > 0) {
+            scrollplay();
+          }
+        })
+      }
+      else{
+        navBar.className = 'nav-bar';
+      }
+    }
 
 
     // see more and see less button event listener
     const textContent = document.querySelector('.body-content');
 
-    textContent.addEventListener('click', function(event){
-      const targetContent = event.target;
+    textContent.addEventListener('click', function(e){
+      const targetContent = e.target;
 
       if(targetContent.id == 'see-more-btn'){
         const sectionContainer = targetContent.parentElement;
@@ -48,5 +76,6 @@ document.addEventListener("DOMContentLoaded", function(){
         seeMoreBtn.style.display = 'block';
       }
     });
+
 
 
